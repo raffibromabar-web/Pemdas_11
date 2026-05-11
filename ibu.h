5da64432503bb0c12a7149eedd8826783d43b77b
raffibromabar-web <raffibromabar@gmail.com>
2026-05-11 15:56:17 +0700

    membuat struktur untuk menampung anak

--- ibu.h
+++ ibu.h
@@ -2,3 +2,17 @@
 #define IBU_H
 #include <vector>
 
+class ibu {
+public:
+    string nama;
+    vector<anak*> daftar_anak;
+
+    ibu(string pNama) :nama (pNama) {
+        cout << "Ibu \"" << nama << "\" ada\n";
+}
+~ibu() {
+    cout << "Ibu \"" << nama << "\"tidak ada\n";
+}
+void tambahAnak (anak*);
+void cetakAnak();
+};
