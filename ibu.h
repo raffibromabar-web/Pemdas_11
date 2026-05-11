2dc867e934cf7a2789d30388217e24091c36263e
raffibromabar-web <raffibromabar@gmail.com>
2026-05-11 15:58:54 +0700

    fungsi untuk menampilkan seluruh nama anak

--- ibu.h
+++ ibu.h
@@ -25,3 +25,9 @@ void ibu::cetakAnak() {
 /*for (auto& a daftar_anak) {
 cout << a->nama << "\n";
 }*/
+for (int i = 0; i < daftar_anak.size(); i++) {
+    cout << daftar_anak[i]->nama << endl;
+}
+    cout << endl;
+}
+#endif
