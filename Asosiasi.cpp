b44d95d5cc5fd7dcdafb35f6782a641dcfb6d5ab
raffibromabar-web <raffibromabar@gmail.com>
2026-05-11 16:52:21 +0700

    fungsi untuk menampilkan daftar dokter dari seorang pasien

--- Asosiasi.cpp
+++ Asosiasi.cpp
@@ -36,3 +36,11 @@ public:
 void pasien::tambahDokter (dokter* pDokter) {
     daftar_dokter.push_back(pDokter);
 }
+
+void pasien::cetakDokter() {
+    cout << "Daftar Dokter yang menangani pasien \"" << this->nama << "\":\n";
+    for (auto& a : daftar_dokter) { // auto digunakan dalam perulangan for untuk secara otomatis menentukan tipe data dari elemen yang diiteras
+    cout << a->nama << "\n";
+    }
+    cout << endl;
+}
